6c9047b688f39e2ec0a8b5fb502c2c6b35474506
javink <javin2861@gmail.com>
2024-10-18 19:10:11 -0400

    implemented restful microservice

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const port = 3000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+
 // **************************************************************
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
